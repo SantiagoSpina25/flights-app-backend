8655234b0d10f1b1e046f05c69827c25a6cbea4a
Santiago Spina <santiagospina025@gmail.com>
2025-09-19 13:12:04 +0200

    HttpRequestMethodNotSupportedException agregada + actualizacion spring boot

--- src/main/java/com/santiago/flightsapp/flights_app/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/santiago/flightsapp/flights_app/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,8 @@ package com.santiago.flightsapp.flights_app.exceptions;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,48 +13,59 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     // Excepcion que ocurre cuando no se encuentra un usuario con el id pasado por
     // parametros
-    @ExceptionHandler({UserNotFoundException.class, FlightNotFoundException.class, AirlineNotFoundException.class})
+    @ExceptionHandler({ UserNotFoundException.class, FlightNotFoundException.class, AirlineNotFoundException.class })
     public ResponseEntity<ErrorResponse> idNotfound(Exception e) {
         ErrorResponse errorResponse = new ErrorResponse(404, "Error id no encontrado", e.getMessage());
 
         return ResponseEntity.status(404).body(errorResponse);
     }
 
-    //Excepcion que ocurre cuando se pasa un parametro con un tipo erroneo
+    // Excepcion que ocurre cuando se pasa un parametro con un tipo erroneo
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorResponse> argumentMismatch(MethodArgumentTypeMismatchException e){
-        String message = String.format("El parametro '%s' con el valor '%s' no es valido. Se esperaba un tipo '%s'", 
-        e.getName(),
-        e.getValue(),
-        e.getRequiredType() != null ? e.getRequiredType().getSimpleName() : "desconocido");
+    public ResponseEntity<ErrorResponse> argumentMismatch(MethodArgumentTypeMismatchException e) {
+        String message = String.format("El parametro '%s' con el valor '%s' no es valido. Se esperaba un tipo '%s'",
+                e.getName(),
+                e.getValue(),
+                e.getRequiredType() != null ? e.getRequiredType().getSimpleName() : "desconocido");
 
         ErrorResponse errorResponse = new ErrorResponse(400, "Bad request", message);
 
         return ResponseEntity.status(400).body(errorResponse);
-        
+
     }
 
-    //Excepcion que ocurre cuando hay un error en la validacion de una entidad
+    // Excepcion que ocurre cuando hay un error en la validacion de una entidad
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<MultipleErrorsResponse> validationError(MethodArgumentNotValidException e) {
-        Map<String,String> errors = new HashMap<>();
-        e.getFieldErrors().forEach(er->{
+        Map<String, String> errors = new HashMap<>();
+        e.getFieldErrors().forEach(er -> {
             errors.put(er.getField(), "Error en el campo '" + er.getField() + "', " + er.getDefaultMessage());
         });
-        MultipleErrorsResponse errorResponse = new MultipleErrorsResponse(400, errors, "Los argumentos introducidos no son validos");
+        MultipleErrorsResponse errorResponse = new MultipleErrorsResponse(400, errors,
+                "Los argumentos introducidos no son validos");
 
         return ResponseEntity.status(400).body(errorResponse);
     }
 
-    //Excepcion que ocurre cuando se pone un endpoint erroneo
+    // Excepcion que ocurre cuando se pone un endpoint erroneo
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<ErrorResponse> resourceNotFound(NoResourceFoundException e) {
         ErrorResponse errorResponse = new ErrorResponse(404, "Error recurso no encontrado", e.getMessage());
 
         return ResponseEntity.status(404).body(errorResponse);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> methodNotSupported(HttpRequestMethodNotSupportedException e) {
+        String message = String.format("El metodo '%s' no esta permitido en este endpoint. Metodos soportados: %s",
+                e.getMethod(), e.getSupportedHttpMethods() != null ? e.getSupportedHttpMethods() : "ninguno");
+
+        ErrorResponse errorResponse = new ErrorResponse(405, "Error metodo erroneo", message);
+
+        return ResponseEntity.status(405).body(errorResponse);
     }
 
 }
