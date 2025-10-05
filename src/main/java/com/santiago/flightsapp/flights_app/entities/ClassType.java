170cb05b4cbe54d311c499088a305f502dc90d3f
Santiago Spina <santiagospina025@gmail.com>
2025-10-05 11:51:21 +0200

    Entidad "Seat" agregada

---
+++ src/main/java/com/santiago/flightsapp/flights_app/entities/ClassType.java
@@ -0,0 +1,5 @@
+package com.santiago.flightsapp.flights_app.entities;
+
+public enum ClassType {
+    ECONOMY, BUSINESS, FIRST_CLASS
+}
---
+++ src/main/java/com/santiago/flightsapp/flights_app/entities/Seat.java
@@ -0,0 +1,40 @@
+package com.santiago.flightsapp.flights_app.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Table(name = "seats")
+@Data
+public class Seat {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String number;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ClassType classType = ClassType.ECONOMY; //Por defecto Economy
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.AVAILABLE; // Por defecto disponible
+
+    @ManyToOne
+    @JoinColumn(name = "flight_id")
+    private Flight flight;
+}
